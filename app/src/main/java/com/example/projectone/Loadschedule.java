cdc9f706c5033a9097ffe27288e52ec5a1213708
babbb06 <joshua.balaoro4@gmail.com>
2024-04-20 22:18:19 +0800

    FeederWireFourth

--- app/src/main/java/com/example/projectone/Loadschedule.java
+++ app/src/main/java/com/example/projectone/Loadschedule.java
@@ -3057,7 +3057,14 @@ public class Loadschedule extends AppCompatActivity {
                 AlertDialog.Builder builder = new AlertDialog.Builder(Loadschedule.this);
                 builder.setView(dialogView)
                         .setTitle("Update Feeder Wire")
-                        .setPositiveButton("OK", null) // Set null initially, we'll enable/disable it later
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Get the selected item from the AutoCompleteTextView
+                                String selectedFeederWire = autoCompleteTextView.getText().toString();
+                                handleFeederWirethirdSelection(selectedFeederWire);
+                            }
+                        })// Set null initially, we'll enable/disable it later
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 // User clicked Cancel button, dismiss the dialog
@@ -3091,26 +3098,7 @@ public class Loadschedule extends AppCompatActivity {
                     }
                 });
 
-                positiveButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Get the selected item from the AutoCompleteTextView
-                        String selectedFeederWire = autoCompleteTextView.getText().toString();
-                        // Update the FeederWire with the new text
-                        FeederWireFourth.setText(" (G)In " + selectedFeederWire + " mmø");
 
-                        String topText = "USE " + FeederWire.getText().toString()+ "\n" + FeederWireSecond.getText().toString()+ "\n" + FeederWireFourth.getText().toString() + Pipetype.getText().toString();
-
-                        TextView[] topViews = {num2_top,num4_top, num6_top, num8_top, num10_top, num12_top, num14_top, num16_top, num18_top, num20_top, num22_top, num24_top, num26_top, num28_top, num30_top};
-
-                        for (int i = 0; i < topViews.length; i++) {
-                            topViews[i].setText(topText);
-                        }
-
-                        // Dismiss the dialog after OK button is clicked
-                        dialog.dismiss();
-                    }
-                });
             }
         });
 
@@ -3356,6 +3344,62 @@ public class Loadschedule extends AppCompatActivity {
         }
     }
 
+    private void handleFeederWirethirdSelection(String selectedFeederWire) {
+        String feed2 = FeederWireFourth.getText().toString();
+
+        if (feed2.startsWith("(G)In ")) {
+            // Extract wire size from feed
+
+            int startIndex = "(G)In ".length();
+            int endIndex = feed2.indexOf(" mmø");
+            String feedWireSizeStr = feed2.substring(startIndex, endIndex);
+
+            try {
+                // Convert feed wire size to double for comparison
+                double feedWireSize2 = Double.parseDouble(feedWireSizeStr);
+
+                // Convert selected feeder wire size to double for comparison
+                double selectedWireSize2 = Double.parseDouble(selectedFeederWire);
+
+
+                // Show dialog message if selected wire size is less than feed wire size
+                if (selectedWireSize2 < feedWireSize2) {
+                    new AlertDialog.Builder(Loadschedule.this)
+                            .setMessage("Please be informed that reducing the wire size significantly may violate the Philippine Electrical Code (PEC) standards.\n\nClick Proceed to confirm if you wish to continue editing.")
+                            .setPositiveButton("Proceed", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    FeederWireFourth.setText("(G)In " + selectedFeederWire + " mmø");
+
+                                    String topText = "USE " + FeederWire.getText().toString()+ "\n" + FeederWireSecond.getText().toString()+ "\n" + FeederWireFourth.getText().toString() + Pipetype.getText().toString();
+
+                                    TextView[] topViews = {num2_top,num4_top, num6_top, num8_top, num10_top, num12_top, num14_top, num16_top, num18_top, num20_top, num22_top, num24_top, num26_top, num28_top, num30_top};
+
+                                    for (int i = 0; i < topViews.length; i++) {
+                                        topViews[i].setText(topText);
+                                    }
+
+                                }
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .show();
+                } else {
+                    FeederWireFourth.setText("(G)In " + selectedFeederWire + " mmø");
+
+                    String topText = "USE " + FeederWire.getText().toString()+ "\n" + FeederWireSecond.getText().toString()+ "\n" + FeederWireFourth.getText().toString() + Pipetype.getText().toString();
+
+                    TextView[] topViews = {num2_top,num4_top, num6_top, num8_top, num10_top, num12_top, num14_top, num16_top, num18_top, num20_top, num22_top, num24_top, num26_top, num28_top, num30_top};
+
+                    for (int i = 0; i < topViews.length; i++) {
+                        topViews[i].setText(topText);
+                    }
+
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
 
@@ -3727,7 +3771,7 @@ public class Loadschedule extends AppCompatActivity {
             }
         }
         else if (FeederW2.equals("2 - 250.0mm\u00B2 THHN/THWN-2 Cu. Wire")) {
-            FeederWireFourth.setText("(G)In 80  mmø");
+            FeederWireFourth.setText("(G)In 80 mmø");
             String botText = "GEC: + 1 - 50.0mm\u00B2 THHN/THWN-2 Cu. Wire";
 
             TextView[] botViews = {num2_bot,num4_bot, num6_bot, num8_bot, num10_bot, num12_bot, num14_bot, num16_bot, num18_bot, num20_bot, num22_bot, num24_bot, num26_bot, num28_bot, num30_bot};

26f70cbee921aa0d7433eebd2f52deb7ca843d06
Angelo Cagas <gelocagas11@gmail.com>
2026-03-09 22:22:01 +0800

    Migrate test stubs to com.angelodev.helpapp

---
+++ app/src/test/java/com/angelodev/helpapp/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.angelodev.helpapp
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
